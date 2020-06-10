01faeff3c823292aad5ba7754fe7433df4b1c818
root <root@p-fgmhjlkizltr-workstation.c.instruqt-prod.internal>
2020-06-10 20:57:32 +0000

    testing

--- vpc.tf
+++ vpc.tf
@@ -4,23 +4,6 @@ data "aws_security_group" "default" {
 }
 
 module "vpc" {
-  source = "../../"
-
-  name = "simple-vpc-example"
-
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_ipv6 = true
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  public_subnet_tags = {
-    Name = "overridden-name-public"
-  }
+  source  = "app.terraform.io/cyrilv-training/vpc/aws"
+  version = "2.39.0"
 }
-
