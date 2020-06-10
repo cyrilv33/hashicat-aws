9ec1a51707b6a917edca716a0919f2010c24fe9e
root <root@p-fgmhjlkizltr-workstation.c.instruqt-prod.internal>
2020-06-10 21:48:23 +0000

    multi regional vpc

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,13 @@
 provider "aws" {
   version = "~> 2.0"
-  region  = var.region
+  alias = "us-west-1"
+  region = us-west-1
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.9.0"
+  providers {
+      aws = "aws.us-west-1"
+  }
 }
