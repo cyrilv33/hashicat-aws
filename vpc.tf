2012d0f46d32b17cdc3afee81c5885b97cd411ec
root <root@p-fgmhjlkizltr-workstation.c.instruqt-prod.internal>
2020-06-10 21:26:18 +0000

    remove vpc module

--- vpc.tf
+++
@@ -1,9 +0,0 @@
-data "aws_security_group" "default" {
-  name   = "default"
-  vpc_id = module.vpc.vpc_id
-}
-
-module "vpc" {
-  source  = "app.terraform.io/cyrilv-training/vpc/aws"
-  version = "2.39.0"
-}
